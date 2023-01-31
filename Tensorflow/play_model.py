97601f77020b708be2cee2c8e8bf49fb82efa7a6
Mongkol39541 <65990036+Mongkol39541@users.noreply.github.com>
2023-02-01 04:26:29 +0700

    up

--- Tensorflow/play_model.py
+++ Tensorflow/play_model.py
@@ -1,36 +1,21 @@
-# How to load a Tensorflow model using OpenCV
-# Jean Vitor de Paulo Blog - https://jeanvitor.com/tensorflow-object-detecion-opencv/
- 
 import cv2
- 
-# Load a model imported from Tensorflow
-tensorflowNet = cv2.dnn.readNetFromTensorflow('frozen_inference_graph.pb', 'graph.pbtxt')
- 
-# Input image
-img = cv2.imread('img.png')
-rows, cols, channels = img.shape
- 
-# Use the given image as input, which needs to be blob(s).
-tensorflowNet.setInput(cv2.dnn.blobFromImage(img, size=(300, 300), swapRB=True, crop=False))
- 
-# Runs a forward pass to compute the net output
-networkOutput = tensorflowNet.forward()
- 
-# Loop on the outputs
-for detection in networkOutput[0,0]:
-    
-    score = float(detection[2])
-    if score > 0.2:
-     
-        left = detection[3] * cols
-        top = detection[4] * rows
-        right = detection[5] * cols
-        bottom = detection[6] * rows
- 
-        #draw a red rectangle around detected objects
-        cv2.rectangle(img, (int(left), int(top)), (int(right), int(bottom)), (0, 0, 255), thickness=2)
- 
-# Show the image with a rectagle surrounding the detected objects 
-cv2.imshow('Image', img)
-cv2.waitKey()
-cv2.destroyAllWindows()
+import mediapipe as mp
+import matplotlib.pyplot as plt
+import numpy as np
+
+img = cv2.imread('image.jpg')
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+mp_draw = mp.solutions.drawing_utils
+
+image_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+results = hands.process(image_rgb)
+if results.multi_hand_landmarks:
+        for landmark in results.multi_hand_landmarks:
+            mp_draw.draw_landmarks(img, landmark, mp_hands.HAND_CONNECTIONS)
+
+img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+plt.imshow(img)
+plt.show()
